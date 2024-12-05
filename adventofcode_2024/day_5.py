09da50933e7cb4698ee4a37b3e050d2664dd2a63
Seb <seb.harrevelt@politie.nl>
2024-12-05 16:42:20 +0100

    small stuff

--- adventofcode_2024/day_5.py
+++ adventofcode_2024/day_5.py
@@ -17,3 +17,23 @@ _ = helper.fetch_test_data(DAY)
 # read input
 puzzle_input = helper.read_lines_strip(DDATA_DAY)
 test_puzzle_input = helper.read_lines_strip(DDATA_DAY_TEST)
+
+chosen_puzzle = test_puzzle_input
+rules = []
+updates = []
+proces_rule = True
+for item in chosen_puzzle:
+    if proces_rule:
+        rules.append(item)
+    else:
+        updates.append(item)
+
+    if item == '':
+        proces_rule = False
+
+rules_int = [helper.int_str2list(x, sep="|") for x in rules if x]
+rules_set = [set(x) for x in rules_int]
+
+for update in updates:
+    update_int = helper.int_str2list(update, sep=',')
+    update_set = set(update_int)
