fccd18eee4671d4826bc4f299fab1fc660e229be
Seb <seb.harrevelt@politie.nl>
2025-12-19 10:54:18 +0100

    day 3 is done :)

--- src/aoc/day_3.rs
+++ src/aoc/day_3.rs
@@ -3,31 +3,43 @@ use crate::utils::file;
 use std::env;
 use crate::aoc::day_3;
 use crate::utils::file::argsort;
+use std::cmp::Ord;
+use std::fmt::{Debug, Display};
+use num_traits::One;
 
-fn form_stuff(data: &Vec<u32>, n_digit: usize, total: u32) -> u32 {
-    // So it is more normal to pass a slice than a vec here...
-    // But what -is- a &Vec? Isnt that 'just' a slice?
-    // So what we gonna do...
+pub fn form_stuff<T>(data: &[T], n_digit: usize, total: T) -> T
+where
+    T: Copy + One + Display  + Debug + Ord + std::ops::Add<Output = T> + std::ops::Mul<Output = T> + From<u32>,
+{
+    println!("Incoming data {:?}", data);
+    println!("Total {:?}", total);
+    println!("N digit {:?}", n_digit);
     // Get the max of the first len(data) - N entries
     // Then recall this function for the remaining entries
-    println!("Incomming data {:?}", data);
-    println!("Number of digits {:?}", n_digit);
-    println!("Current total {:?}", total);
     if n_digit == 0 {
+        println!("Finished");
         return total;
     }
-    let n = data.len() as usize;
-    let data_subet = &data[0..n - n_digit];
-    println!("data subset: {:?}", data_subet);
-    let max_value = data_subet.iter().max().unwrap();
+    let n = data.len();
+    let data_subet = &data[0..n - (n_digit - 1)];
+    println!("Data subet {:?}", data_subet);
+    let max_value = data_subet.iter().max().unwrap().to_owned();
+    println!("Max value {:?}", max_value);
     let max_index = data_subet
         .iter()
-        .position(|x| x == max_value)
+        .position(|x| x == &max_value)
         .unwrap();
-    println!("index {:?}", max_index);
-    let new_data = &data[max_index+1 .. n].to_vec();
-    let delta = (max_value) * 10_u32.pow((n_digit - 1) as u32);
-    form_stuff(new_data, n_digit-1, total + delta)
+    println!("Max index {:?}", max_index);
+    // Now, we slice the data vector to the remaining digits
+    // And then we are going to call this function again
+    let new_data = &data[max_index+1 .. ].to_vec();
+    let pow10 = (0..(n_digit - 1))
+        .fold(T::one(), |acc, _| acc * T::from(10u32));
+    let new_total = total + max_value * pow10;
+    println!("New total {:?}", new_total);
+    println!("New data {:?}", new_data);
+
+    form_stuff(new_data, n_digit-1, new_total)
 }
 
 #[allow(dead_code)]
@@ -74,6 +86,12 @@ pub fn part_2(){
     let loaded_data: _ = file::load_data(datapath, file_seperator);
     println!("{:?}", loaded_data);
     let data: _ = file::vec_string_to_int::<u64>(loaded_data);
-    println!("----------------");
-    form_stuff(&data[0], 12, 0);
+    let mut total = 0;
+    for line in data {
+        println!("----------------------");
+        let result = form_stuff::<u64>(&line, 12, 0);
+        total += result;
+    }
+    println!("{:?}", total);
+
 }
